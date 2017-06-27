33e278c6ee1d84609a05641265e420a790b41382
drangob <drangob123@gmail.com>
2017-06-27 11:53:03 +0100

    Write some code to verify that both aes candidates are working as expected

--- src/aesCTR/workingTest.c
+++ src/aesCTR/workingTest.c
@@ -1,32 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
-#include <string.h>
-#include <stdint.h>
-#include <stdio.h>
 
-void print_uint128(__uint128_t n)
-{
-    if (n == 0) {
-      return;
-    }
+#include "aes.h"
 
-    print_uint128(n/10);
-    putchar(n%10+0x30);
-}
+
 
 int encrypt(__uint128_t *ctr, unsigned char *key, unsigned char *output);
 
-void handleErrors(void){}
+void handleErrors(void){
+  puts("Error occurred");
+  exit(-1);
+}
 
-int main (void) {
-
-  FILE *fd = fopen("data.bin", "wb");
-
-  /* Set up the key and iv. Do I need to say to not hard code these in a
-   * real application? :-)
-   */
-
+int openSSL(__uint128_t *_input, __uint128_t *_output) {
   /* A 128 bit key */
   unsigned char *key = (unsigned char *)"0123456789012345";
 
@@ -46,13 +37,10 @@ int main (void) {
   OpenSSL_add_all_algorithms();
   OPENSSL_config(NULL);
 
-  for (int i = 0; i < 1; ++i) {
-    ciphertext_len = encrypt (&ctr, key, output);
-    ctr++;
-
-    fwrite(output, ciphertext_len, 1, fd);
-  }
-
+  
+  ciphertext_len = encrypt (&ctr, key, output);
+  *_input = ctr;
+  *_output = output[0];
   /* Clean up */
   EVP_cleanup();
   ERR_free_strings();
@@ -88,4 +76,43 @@ int encrypt(__uint128_t *ctr, unsigned char *key, unsigned char *output)
   EVP_CIPHER_CTX_free(context);
 
   return ciphertext_len;
+}
+
+
+int tinyAES(__uint128_t *_input, __uint128_t *_output) {
+
+	__uint128_t counter = 0;
+
+	//library needs 16 bytes 
+	uint8_t input[16];
+	uint8_t output[16];
+	/* A 128 bit key */
+  	unsigned char *key = (unsigned char *)"0123456789012345";
+
+	//encrypt 0 with 012345789012345
+	AES128_ECB_encrypt((const unsigned char *)&counter, key, output);
+
+	*_input = counter;
+	*_output = (__uint128_t)output[0];
+	return 0;
+}
+
+
+
+
+
+int main(int argc, char const *argv[]) {
+	__uint128_t openSSLin, openSSLout;
+	__uint128_t tinyAESin, tinyAESout;
+
+	//provide containers for the input and output of openssl implementation
+	openSSL(&openSSLin, &openSSLout);
+
+	//provide containers for the input and output of tinyAES
+	tinyAES(&tinyAESin, &tinyAESout);
+
+
+	if(openSSLin == tinyAESin && openSSLout == tinyAESout)
+		puts("all good!");
+	return 0;
 }
