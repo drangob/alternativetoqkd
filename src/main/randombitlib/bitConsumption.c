14e03a781b167edae4668822988e974367d8e339
drangob <drangob123@gmail.com>
2017-08-31 15:24:28 +0100

    Revert "Fastfoward was exiting, bad."
    This reverts commit 90994f67c8b719411d3fd353ae20cf827c23758b.

--- src/main/randombitlib/bitConsumption.c
+++ src/main/randombitlib/bitConsumption.c
@@ -133,6 +133,6 @@ char *getBytes(char *path, struct pointerFile *ptr, uint32_t numOfBytes) {
 }
 
 char *getBytesWithFastForward(char *path, struct pointerFile *ptr, uint32_t numOfBytes, uint32_t fileNumber, uint32_t offset) {
-	if(fastForwardPtr(ptr, fileNumber, offset)) return NULL;
+	if(fastForwardPtr(ptr, fileNumber, offset)) exit(-1);
 	return getBytes(path, ptr, numOfBytes);
 }
